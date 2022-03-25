a2fdb5e6204444d797fe69b96ec69540772d9713
Aurélien MEUNIER <83410534+Aurelien71@users.noreply.github.com>
2022-03-25 10:32:54 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -2,5 +2,22 @@
 import PackageDescription
 
 let package = Package(
-    name: "iCalKit"
+    name: "ICalKit",
+    platforms: [
+      .iOS(.v13),
+      .macOS(.v11)
+    ],
+    products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
+            name: "ICalKit",
+            targets: ["ICalKit"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(
+            name: "ICalKit",
+            path: "Sources"),
+    ]
 )
